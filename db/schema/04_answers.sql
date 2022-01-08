b3d1ff795420e97790accda6fce93179dd325fb5
Adrian Kiva <akiva@mta.ca>
2022-01-08 14:30:47 -0500

    import erd begin schema

---
+++ db/schema/04_answers.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS answers CASCADE;
+CREATE TABLE answers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  text VARCHAR(255) NOT NULL,
+  correct_answer BOOLEAN NOT NULL DEFAULT false
+);
