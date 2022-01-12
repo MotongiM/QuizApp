bb663d04bc0978c56217de5f8c48ee3883f56ed4
Michel <Michel_motongi@hotmail.com>
2022-01-12 07:30:16 -0500

    fix merge conflict

--- routes/users.js
+++ routes/users.js
@@ -30,34 +30,54 @@ module.exports = (db) => {
   router.get('/:id',(req,res) => {
     const templateVars = {};
 
-    db.query(`SELECT username, id
-              FROM users`)
-              .then(templateVars.username = user,
-                    templateVars.id = id);
+    const correctCount = `SELECT count(attempt_answers.id)
+    FROM answers
+    JOIN attempt_answers ON answers.id = attempt_answers.answer_id
+    JOIN attempts ON attempts.id = attempt_answers.attempt_id
+    WHERE attempts.user_id = $1
+    AND answers.correct_answer = TRUE
+    GROUP BY attempts.id
+    ORDER BY attempts.id
+    ;`;
 
-    db.query(`Select answer/`)
+    const totalCount = `SELECT count(attempt_answers.id)
+    FROM attempt_answers
+    JOIN attempts ON attempts.id = attempt_answers.attempt_id
+    WHERE attempts.user_id = $1
+    GROUP BY attempts.id
+    ORDER BY attempts.id
+    ;`;
 
-    db.query(`SELECT Quizzes_id, users.id as user_id,users.username AS users_name, Quizzes.title, Quizzes.description,public
-    FROM Quizzes
-    JOIN users ON user_id = users.id
-    JOIN attemps ON
-    WHERE user_id = 1$
-    GROUP BY Quizzes.id, users.id, users.username,Quizzes.title,Quizzes.description,public;`, [req.params.id])
-    .then (user => {
-      const templateVars = {userData: user.rows, user_id: req.params.id };
-      res.render("../views/account", templateVars);
-    })
-
-    .catch(error => {
-      res
-        .status(500)
-        .json({ error: error.message });
-    });
+    db.query(correctCount, [req.params.id])
+    .then( result => {
+      // templateVars.correctCount = (result.rows[0].count);
+      console.log('correct count:', result.rows);
+      templateVars.correctCount = result.rows;
+      db.query(totalCount, [req.params.id])
+      .then(result => {
+        // templateVars.totalCount = (result.rows[0].count);
+        templateVars.totalCount = result.rows;
+        db.query(`SELECT quizzes.title
+        FROM quizzes
+        JOIN attempts ON attempts.quiz_id = quizzes.id
+        WHERE attempts.user_id = $1
+        ORDER BY attempts.id;`, [req.params.id])
+        .then(result => {
+          templateVars.quizNames = result.rows;
+          templateVars.user_id = req.params.id;
+          console.log('quiz names: ', result.rows);
+          console.log({templateVars});
+          res.render("../views/account", templateVars);
+        })
+      })
+      })
   });
+
+  //POST a new quiz to the database
 
   router.post('/user_id/createquiz',(req,res) => {
     const query = `INSERT INTO quizzes (user_id, title, description,public)
-                 VALUES (1$,2$,3$,4$) RETURNING id`;
+                 VALUES ($1,$2,$3,$4) RETURNING id`;
     const values = [req.params.user_id, req.body.title, req.body.description, req.body.public];
     db.query(query,values)
       .then(data => {
@@ -70,8 +90,11 @@ module.exports = (db) => {
       });
   });
 
+  //POST delete a new quiz
+  //stretch instead of delete add a boolean for active or not
+
   router.post('/:user_id/delete/:quiz', (req,res) => {
-    db.query(`DELETE FROM Quizzes WHERE id = 1$ AND user_id = 2$`)
+    db.query(`DELETE FROM Quizzes WHERE id = $1 AND user_id = $2`)
   })
 
   return router;
