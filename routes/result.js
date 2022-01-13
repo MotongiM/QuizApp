c92df4b953bcc2410cc72197b11e18ec79913ff0
Michel <Michel_motongi@hotmail.com>
2022-01-13 18:43:27 -0500

    Fixed the logic for the result page

--- routes/result.js
+++ routes/result.js
@@ -2,21 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
+  // router.get('/', (req, res) => {``
 
+  // const templateVars = {user_id: req.params.user_id};
+  // res.render('../views/result', templateVars);
+  // })
   //result page
 
-  router.get('/:attempts/results', (req,res) => {
-    db.query(`SELECT attempt_answers.id,answers.answer,questions.question,correct_answer,attempt_id,attempts.user_id
+  router.get('/:quizId/result', (req,res) => {
+    const quizId = req.params.quizId;
+    db.query(`SELECT attempt_answers.id,answers.answer,questions.question,correct_answer,attempt_id,attempts.user_id,quizzes.title
     FROM attempts
     JOIN attempt_answers ON attempts.id = attempt_id
     JOIN answers ON answers.id = answer_id
     JOIN quizzes ON attempts.quiz_id = quizzes.id
     JOIN questions ON questions.id = answers.question_id
-    WHERE attempts.id = $1
-    ORDER BY id;`, [1])
+    WHERE quizzes.id = $1
+    ORDER BY quizzes.id;`, [quizId])
     .then (data => {
+      console.log('check ', data);
       const templateVars = {attempts: data.rows}
-      res.render('../views/result',templateVars);
+      res.render('../views/result.ejs', templateVars);
     })
     .catch(error => {
       res
@@ -27,7 +33,7 @@ module.exports = (db) => {
 
 
   //Inserting data into database
-  router.post('/:quiz_id/results/:user_id' ,(req,res) => {
+  router.post('/:quiz_id/result/:user_id' ,(req,res) => {
     const query = `INSERT INTO attempts (quiz_id, user_id)
                   VALUES ($1, $2)
                   RETURNING *;`
