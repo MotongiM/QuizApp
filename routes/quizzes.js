03f9af12731dc75625b9531b546d40b919a36956
Michel <Michel_motongi@hotmail.com>
2022-01-13 05:16:08 -0500

    Added a quiz routes

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
-  router.get('/:quizid')
 
+  // quiz in progress
+
+  router.get('/:quizid', (req, res) => {
+    db.query(`SELECT user_id, question_id, quizzes.title,questions.question, answers.answer, quiz_id, answers.correct_answer
+              FROM answers
+              JOIN questions ON questions.id = answer.question_id
+              JOIN quizzes ON quizzes.id = questions.quiz_id
+              JOIN users ON user_id = users.id
+              WHERE quiz_id = $1
+              GROUP BY user_id, question_id, quizzes.title, questions.question, answers.answer, quiz_id, answers.correct_answer
+              ORDER BY questions.id;`, [req.params.quizid])
+                .then(data => {
+                  const templateVars = {quizInput: data.rows}
+                  res.render('../views/takeQuiz', templateVars)
+                });
+  });
+  return router;
 };
