e66d38010262deca5c4fdce34d1f426d1aa475f8
Michel <Michel_motongi@hotmail.com>
2022-01-13 05:20:28 -0500

    Added a quiz Route

--- server.js
+++ server.js
@@ -35,14 +35,20 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
+const usersRoutes = require("./routes/account");
 const widgetsRoutes = require("./routes/widgets");
 const indexRoutes = require("./routes/index");
+const createQuizRoutes = require("./routes/createQuiz");
+const resultRoutes = require("./routes/result");
+const quizRoutes = require("./routes/takeQuiz")
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 app.use("/",indexRoutes(db));
+app.use("/create",createQuizRoutes(db));
+app.use("/result",resultRoutes(db));
+app.use("/quiz", quizRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
