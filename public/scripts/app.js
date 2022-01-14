24b95570aeb8caa20f020d31bba360e79d535e1e
Adrian Kiva <akiva@mta.ca>
2022-01-14 10:09:19 -0500

    implement clipboardjs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,10 @@
 // Client facing scripts here
 $(() => {
-  console.log('loaded');
   $login = $('#login');
   // document.cookie;
-  console.log(document.cookie);
   $(document).cookie ? $login.replaceWith(`<span>No cookies</span>`) : $login.replaceWith(`<a href='/user/'>User ${document.cookie.slice(8)}</a>`);
+
+  new ClipboardJS('.share');
 
 
 })
