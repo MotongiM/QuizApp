71efb7e8e6c106b834e2d0088889aba2ff2eaca9
Adrian Kiva <akiva@mta.ca>
2022-01-14 08:01:08 -0500

    change friends quiz seed

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -3,4 +3,4 @@ VALUES
 (1, 'Canada history', 'Do you know the history of Canada?', TRUE),
 (1, 'America history', 'Do you know the history of America?', FALSE),
 (2, 'Capitals', 'Name the capitals', TRUE),
-(3, 'Can you guess the names of all the actors from the hit TV show Friends?', 'I will be there for you!', TRUE);
+(3, 'Can you guess the names of all the actors from the hit TV show Friends?', 'Who will be there for you!', TRUE);
