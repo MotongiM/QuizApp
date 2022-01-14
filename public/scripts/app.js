37f2d7750067154b2bd8e0112cb4094da0ec8bd7
Adrian Kiva <akiva@mta.ca>
2022-01-14 07:58:13 -0500

    reformat user name on home page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@ $(() => {
   $login = $('#login');
   // document.cookie;
   console.log(document.cookie);
-  $(document).cookie ? $login.replaceWith(`<span>No cookies</span>`) : $login.replaceWith(`<a href='/user/'>${document.cookie}</a>`);
+  $(document).cookie ? $login.replaceWith(`<span>No cookies</span>`) : $login.replaceWith(`<a href='/user/'>User ${document.cookie.slice(8)}</a>`);
 
 
 })
